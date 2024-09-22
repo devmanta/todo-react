4f3bbd307e42f116ee0377cb54b01a42d404969b
heejin <dlwlrgkwk82@gmail.com>
2024-09-22 23:15:56 +0900

    feat: add todo list css

--- src/components/Todo/Todo.jsx
+++ src/components/Todo/Todo.jsx
@@ -12,17 +12,22 @@ export default function Todo({ todo, onDelete, onUpdate }) {
   };
 
   return (
-    <div>
+    <div className={styles.todo}>
       <input
         type="checkbox"
         id={id}
         checked={status === 'completed'}
         onChange={handleOnChange}
+        className={styles.checkbox}
       />
-      <label htmlFor={id}>{content}</label>
-      <button onClick={handleOnDelete}>
-        <FaTrashAlt />
-      </button>
+      <label htmlFor={id} className={styles.content}>
+        {content}
+      </label>
+      <span className={styles.icon}>
+        <button onClick={handleOnDelete} className={styles.button}>
+          <FaTrashAlt />
+        </button>
+      </span>
     </div>
   );
 }
